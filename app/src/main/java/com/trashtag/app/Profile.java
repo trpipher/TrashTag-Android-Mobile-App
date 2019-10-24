1dcc3ed7f1cfdd120f652901e7c37266ff78f993
Shankar Sharma <shankar.sharma@usm.edu>
2019-10-24 12:15:06 -0500

    Bottom Navigation

--- app/src/main/java/com/trashtag/app/Profile.java
+++ app/src/main/java/com/trashtag/app/Profile.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class Profile extends AppCompatActivity {
 
     Button btn_Back;
