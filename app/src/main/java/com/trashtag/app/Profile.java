265b8b6655113a5fc4c2727ed610e92df64653b8
Jansen Howell <jansenhowell@yahoo.com>
2019-11-20 21:48:49 -0600

    put the sign out button on the profile page

--- app/src/main/java/com/trashtag/app/Profile.java
+++ app/src/main/java/com/trashtag/app/Profile.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.maps.SupportMapFragment;
 
 public class Profile extends AppCompatActivity {
@@ -16,6 +21,9 @@ public class Profile extends AppCompatActivity {
     TextView scoreText;
     TextView trashText;
     TextView recycleText;
+    Button btnSignOut;
+    GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +38,34 @@ public class Profile extends AppCompatActivity {
         recycleText.setText(""+User.user.getRecyclePins()+" " + getString(R.string.recyclePlaced));
         trashText.setText(User.user.getTrashPins()+" " + getString(R.string.trashPlaced));
 
+        btnSignOut = findViewById(R.id.signOut2);
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
 
+                mGoogleSignInClient = GoogleSignIn.getClient(getApplicationContext(), gso);
+
+
+                if (AppResources.mAuth().getCurrentUser() == null) {
+                    Toast.makeText(getApplicationContext(), "Not Logged In...", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Firebase sign out
+                    AppResources.mAuth().signOut();
+                    try {
+                        // Google sign out
+                        mGoogleSignInClient.signOut();
+                    } catch (Exception e) {
+                        Log.e("ERROR SIGN OUT", "message", e);
+                    }
+                    Toast.makeText(getApplicationContext(), "Signed Out...", Toast.LENGTH_SHORT).show();
+                }
 
 
         }
+    });
     }
+}
